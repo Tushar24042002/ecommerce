d2292c9a5d08ed7c2a3f0d0e58c9c0a4cccb483a
Tushar <100976552+Tushar24042002@users.noreply.github.com>
2023-11-07 20:37:55 +0530

    Update cart.js

--- router/cart.js
+++ router/cart.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
+const CustomerMiddleware = require("../middleware/CustomerMiddleware");
 const cartController = require('../controller/cartController');
 
 
-router.post('/addToCart', cartController.addToCart);
-router.get('/cartProdcuts',cartController.getCart);
+router.post('/addToCart',CustomerMiddleware, cartController.addToCart);
+router.get('/cartProducts',CustomerMiddleware,cartController.getCart);
+
+router.put('/deleteItemByID',CustomerMiddleware,cartController.deleteItemByID);
 module.exports = router;
