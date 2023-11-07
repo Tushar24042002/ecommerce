85e9efc6bd1ed40c4746ff7279442788bdce0bb2
Tushar <100976552+Tushar24042002@users.noreply.github.com>
2023-11-07 20:34:23 +0530

    Update cartController.js

--- controller/cartController.js
+++ controller/cartController.js
@@ -2,13 +2,18 @@
 const Cart = require('../model/Cart');
 
 // Add a product to the user's cart
-exports.addToCart= async (userId, productId, quantity) =>{
+exports.addToCart = async (req,res) =>{
+console.log(res.user.id)
   try {
+    const userId = req.user.id;
+    const productId = req.body.productId;
+    let quantity = req.body.quantity;
+   
     let cart = await Cart.findOne({ user: userId });
-
+ 
     if (!cart) {
       cart = new Cart({ user: userId, items: [] });
-    }
+   }
 
     const existingCartItem = cart.items.find((item) =>
       item.product.equals(productId)
@@ -16,23 +21,51 @@ exports.addToCart= async (userId, productId, quantity) =>{
 
     if (existingCartItem) {
       existingCartItem.quantity += quantity;
+      if(existingCartItem.quantity <= 0){
+        cart.items = cart.items.filter(item => item !== existingCartItem);
+       
+      }
     } else {
       cart.items.push({ product: productId, quantity });
     }
 
     await cart.save();
-    return cart;
+    res.status(200).json(cart);
+  } catch (error) {
+    throw error;
+  }
+}
+// .populate('items.product')
+// Get the user's cart
+exports.getCart= async(req,res)=> {
+  try {
+    const userId = req.user.id;
+    const cart = await Cart.findOne({ user: userId }).populate('items');
+
+    res.status(200).json(cart);
+
   } catch (error) {
     throw error;
   }
 }
 
-// Get the user's cart
-exports.getCart= async(userId)=> {
-  try {
-    const cart = await Cart.findOne({ user: userId }).populate('items.product');
-    return cart;
-  } catch (error) {
-    throw error;
+
+
+// delete item from cart by id
+exports.deleteItemByID = async(req,res)=>{
+  try{
+    const userId = req.user.id;
+    const cartElementId = req.body.cartProductId;
+    const cart = await Cart.findOne({ user: userId });
+    if(!cart){
+      res.status(500).json({message : "Cart not found for this User"});
+    }
+
+    cart.items = cart.items.filter(item => item._id !== cartElementId);
+    console.log(cart);
+    res.status(200).json(cart);
+
+  }catch(err){
+
   }
 }
